ed094ad582885421af6220de4bf9ff3f29feae3c
victorfmaraujo <victorfmaraujo@gmail.com>
2017-08-09 16:09:09 -0300

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -2,5 +2,5 @@
 mkdir /scripts
 #Localiza ultima versão do Speedtest
 pkg_speedtest="none"
-pkg_speedtest=$(pkg search speedtest | awk '{print $1}')
+pkg_speedtest=$(/usr/sbin/pkg search speedtest | awk '{print $1}')
 pkg install -y $pkg_speedtest
