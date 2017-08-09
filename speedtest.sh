49c1f1fe2929de92292a58264e72fc81726130a3
Victor Franca <victor.franca@ewinfo.com.br>
2017-08-09 13:18:31 -0300

    Primeira Versao

---
+++ speedtest.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#Localiza ultima versão do Speedtest
+pkg_speedtest=pkg search speedtest | awk '{print $1}'
+pkg install -y $pkg_speedtest
+rehash
+mkdir /scripts
+chmod +x /scripts/*
+
+
+
+
+
+
+
